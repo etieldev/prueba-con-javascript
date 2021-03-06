0b7e926add45790d0669a5ffaf1f7725f31c5d8a
etieldev <etielcuentapersonal@gmail.com>
2021-03-06 00:26:38 +0000

    Se añade código de prueba en fichero JS

---
+++ js/comportamiento.js
@@ -0,0 +1,10 @@
+let number = 1;
+
+if (number != 1) {
+	console.log("El número no es igual a 1")
+} else if (number <= 1) {
+	console.log("El número es igual a 1")
+} else {
+	console.log("Estás equivocado")
+};
+
